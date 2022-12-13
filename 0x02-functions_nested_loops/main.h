213e731af593347314c3cd9f2f587079c7eba9c5
ochuko8 <okiuks@gmail.com>
2022-12-13 07:46:57 -0800

    New

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,17 +2,5 @@
 #define MAIN_H
 
 void print_alphabeth(void);
-void print_alphabeth_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void)
-void times_tables(void);
-int add(int, int);
-void print_to_98(int n);
-int _putchar(char c);
-
 
 #endif
