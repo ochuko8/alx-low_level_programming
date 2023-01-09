bec94ed8f593873eb18b976c7eed660778332ba6
ochuko8 <okiuks@gmail.com>
2023-01-09 08:22:12 -0800

    New task

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ * _puts - this function returns 0
+ *@str: str is a parameter
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str);
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
